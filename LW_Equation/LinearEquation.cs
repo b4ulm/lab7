f4dd0db8b146bceb80b444bd93ab07ddcb9415b4
eclat <fedormaltsevm@gmail.com>
2022-06-19 13:03:36 +0300

    feat: add operator -

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -154,5 +154,14 @@ namespace LW_Equation
             return e;
         }
 
+        static public LinearEquation operator -(LinearEquation a)
+        {
+            for (int i = 0; i < a.Size; i++)
+            {
+                a.coefficients[i] *= -1;
+            }
+            return a;
+        }
+
     }
 }
